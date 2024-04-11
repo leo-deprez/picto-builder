42a21ec81ee1599a6ff65da7ecd7e745736851b4
Leo Deprez <leo_deprez@hotmail.fr>
2024-04-11 12:25:47 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import './assets/main.css'
+import './assets/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import './index.css'
 
 const app = createApp(App)
 
