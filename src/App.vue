75c23d921f229f5fe053d9c0a2b0aab5bdd18601
Leo Deprez <leo_deprez@hotmail.fr>
2024-04-11 12:24:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <BaseComponent />
+  </main>
+</template>
+
+<script setup lang="ts">
+import BaseComponent from '@/components/BaseComponent.vue'
+</script>
+./components/BaseComponent.vue
